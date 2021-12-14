522dff3be949bc2bc5e564d58db720c218432234
abdellahi-brahim <79083991+abdellahi-brahim@users.noreply.github.com>
2021-12-14 19:41:01 +0000

    Rooms and Players Listing
    Changed LobbyScene to list avaliable rooms

---
+++ Assets/Scripts/UI/Lobby/CurrentRoomCanvas.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrentRoomCanvas : MonoBehaviour
+{
+    private LobbyCanvases _lobbyCanvases;
+
+    public void FirstInitialize(LobbyCanvases canvases){
+        _lobbyCanvases = canvases;
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
